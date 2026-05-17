14cf3e5bd6018bae2271b28c7991a7e40f3b1037
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-16 18:16:59 -0700

    Complete foundations 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegree) {
+  let celsius = (fahrenheitDegree - 32)* (5/9);
+  let roundedOneDecimal = Math.round(celsius*10) / 10;
+  return roundedOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegree) {
+  let fahrenheit = celsiusDegree* (9/5) + 32;
+  let roundedOneDecimal = Math.round(fahrenheit*10) / 10;
+  return roundedOneDecimal;
 };
 
 // Do not edit below this line
