e16d15ad5b3749b1b3adc6172ee0a1dd9ad18aed
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-16 17:59:05 -0700

    Complete foundations 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(minNum, maxNum) {
+    if (!Number.isInteger(minNum) ||
+        !Number.isInteger(maxNum) ||
+        minNum < 0 || maxNum < 0
+    ) {
+        return "ERROR";
+    }
 
+    if (minNum > maxNum) {
+        let temp = minNum;
+        minNum = maxNum;
+        maxNum = temp;
+    }
+
+    let sum = 0;
+    for (let i = minNum; i <= maxNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
