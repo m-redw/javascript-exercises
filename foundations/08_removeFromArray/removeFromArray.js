71aaaceb643decf30c1a241c1b628a37cbafaa4f
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-16 17:42:19 -0700

    Complete foundations 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removables) {
+    return arr.filter((num) => {
+        for (const arg of removables) {
+            if (num === arg) return false;
+        }
+        return true;
+    });
 };
 
 // Do not edit below this line
