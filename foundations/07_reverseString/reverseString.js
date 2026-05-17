a35fb76f68ad53772557c7d3bf6a23488a91cc58
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-16 17:31:52 -0700

    Complete foundations 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArr = string.split('');
+    let reversedArr = strArr.reverse();
+    let joinedReversed = reversedArr.join('');
+    return joinedReversed
 };
 
 // Do not edit below this line
