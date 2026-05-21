3cea9f8d5f037c161ef684a37ddddbd535c9a278
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-21 16:25:31 -0700

    Complete foundations 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, number) => total + number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((total, number) => total * number, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let ans = 1;
+  for (let i = 1; i <= num; i++) {
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
