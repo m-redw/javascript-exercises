825359058e40fab04f0014afadc8c07736a3a3c7
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-21 18:08:42 -0700

    Complete foundations 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const validChars = 'abcdefghijklmnopqrstuvwxyz1234567890';
 
+    const newString = string
+                        .toLowerCase()
+                        .split('')
+                        .filter(char => validChars.includes(char))
+                        .join('');
+
+    const reverseStr = newString
+                        .split('')
+                        .reverse()
+                        .join('');
+
+    return newString === reverseStr;
 };
 
 // Do not edit below this line
