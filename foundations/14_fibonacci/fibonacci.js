90fbc31a9910e57fc24ba51c6f58ba251cef1e6d
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-21 18:34:17 -0700

    Complete foundations 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof num != 'number') {
+        num = parseInt(num);
+    }
 
+    // test case for === 1 not needed cuz for loop will just
+    // be skipped and func returns fib[1] which is 1.
+    if (num === 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    }
+
+    let fib = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        const sum = fib[i-2] + fib[i-1];
+        fib.push(sum);
+        console.log(fib);
+    }
+    
+    return fib[num];
+    //return fib.findLast((oioioi)=>oioioi);
 };
 
 // Do not edit below this line
