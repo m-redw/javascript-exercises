330e4590621e94d478bbb2cc7c126a8bcae6b2b1
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-21 19:25:52 -0700

    Complete foundations 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldestList = people.toSorted((a, b) => {
+        let person1Age;
+        let person2Age;
 
+        if (a.yearOfDeath) {
+            person1Age = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            person1Age = (new Date()).getFullYear() - a.yearOfBirth;
+        }
+        
+        if (b.yearOfDeath) {
+            person2Age = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            person2Age = (new Date()).getFullYear() - b.yearOfBirth;
+        }
+        
+        if (person1Age < person2Age) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    
+    return oldestList[oldestList.length-1];
 };
 
 // Do not edit below this line
